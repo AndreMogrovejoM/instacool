60d4f1c9fd5ee931e5a90cc1b924c7910fea00e6
Skdi <jddhghf@gmail.com>
2021-03-25 16:18:39 -0500

    first push with all configurations

---
+++ src/components/Intro.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default  class Intro extends React.Component {
+    public render(){
+        return (
+            <p className = "App-intro">
+                LALAL
+            </p>
+        )
+    }
+}
