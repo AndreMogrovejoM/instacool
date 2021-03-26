fb98b4f6ce5ba740ad415c8c93a5df90b2546ef8
Skdi <jddhghf@gmail.com>
2021-03-26 11:52:48 -0500

    installing redux n react redux

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import './App.css';
 import Header from './components/Header'
 import Intro from './components/Intro'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Intro />
-      <Intro />
-    </div>
-  );
+class App extends React.Component {
+  public render(){
+    return (
+      <div className="App">
+        <Header />
+        <Intro />
+        <Intro text="bola"/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react'
 
-export default  class Intro extends React.Component {
+interface IIntroProps{
+    text?: string
+}
+
+interface IIntroState{
+    dato: number
+    text: string
+}
+
+export default  class Intro extends React.Component<IIntroProps,IIntroState> {
+
+    public state = {
+        dato: 1,
+        text: "Texto por defecto"
+    }
+
     public render(){
+        
+        const { text } = this.props
+        const t = text ? text : this.state.text
+
         return (
-            <p className = "App-intro">
-                LALAL
+            <p onClick={this.handleClick} className = "App-intro">
+                <span>{t}</span>
             </p>
         )
+    }
+
+    private handleClick = () =>{
+        this.setState({text: "Actualizado"})
     }
 }
