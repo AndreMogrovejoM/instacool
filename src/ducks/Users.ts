f951b8c613ac2081eb49cbfb5f2478fa82234b5c
Skdi <jddhghf@gmail.com>
2021-03-31 10:28:21 -0500

    valio

--- src/ducks/Users.ts
+++ src/ducks/Users.ts
@@ -12,6 +12,25 @@ export default function reducer(state = {}){
 
 export const login = ({email, password}: ILogin) => 
  async (dispatch: Dispatch, getState: () => any, { auth } : IServices) => {
-    const result = await auth.signInWithEmailAndPassword(email, password)
-    console.log(result)
-}
+    try{
+        const result = await auth.signInWithEmailAndPassword(email, password)
+        console.log(result)
+    } catch(error){
+        console.log("valio")
+    }
+    
+ }
+
+
+/*  export default payload =>
+    async (dispatch, getState) => {
+        dispatch(fetchStart())
+        try {
+            const result = await fetch(url)
+            const json = await result.json()
+            dispatch(fetchSucess(json))
+            console.log(json)
+        } catch (error) {
+            dispatch(fetchError(error))
+        }
+    } */
